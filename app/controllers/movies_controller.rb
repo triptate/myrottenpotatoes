839e8f4a440a2df13e447706eeafd26e32de5a05
Trip Tate <trip@pegulabs.com>
2014-11-28 16:20:31 +0000

    Added views & routes for new & show

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,19 @@
+# This file is app/controller/movies_controller.rb
+
+class MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.html.haml by default
+  end
+
+  def new
+    # default: render 'new' template
+  end
+
+end
