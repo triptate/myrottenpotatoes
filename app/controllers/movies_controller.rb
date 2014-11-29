7eb32f782bd7ca16d823e051dcb20d807b858cc9
Trip Tate <trip@pegulabs.com>
2014-11-29 23:24:16 +0000

    Added the create controller action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,4 +16,10 @@ class MoviesController < ApplicationController
     # default: render 'new' template
   end
 
+  def create
+    @movie = Movie.create!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+
 end
