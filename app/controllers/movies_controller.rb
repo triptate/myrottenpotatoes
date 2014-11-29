1d920274d496e86265e0c65ec7663f15770d758b
Trip Tate <trip@pegulabs.com>
2014-11-29 23:49:11 +0000

    Added edit and update routes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -22,4 +22,15 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(params[:movie])
+    flash[:notice] = "#{movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+
 end
