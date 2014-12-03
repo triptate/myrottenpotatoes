6e6f3e43eea9608d017b85c3432670e545edf8a7
Trip Tate <trip@pegulabs.com>
2014-12-03 05:39:58 +0000

    Added error handling for bad movie ID

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,7 +8,12 @@ class MoviesController < ApplicationController
 
   def show
     id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
+    begin
+      @movie = Movie.find(id) # look up movie by unique ID
+    rescue
+      flash[:warning] = "Movie with ID #{id.to_s} was not found."
+      redirect_to movies_path
+    end
     # will render app/views/movies/show.html.haml by default
   end
 
