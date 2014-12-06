fb350d29b7e67770753580287464e778f4b9e900
Trip Tate <trip@pegulabs.com>
2014-12-06 23:37:04 +0000

    Added movie validation logic to controller and views to prevent bad saves and present nice error messages

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,13 +18,17 @@ class MoviesController < ApplicationController
   end
 
   def new
-    # default: render 'new' template
+    @movie = Movie.new
   end
 
   def create
-    @movie = Movie.create!(params[:movie])
-    flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movie_path(@movie)
+    @movie = Movie.new(params[:movie])
+    if @movie.save
+      flash[:notice] = "#{@movie.title} was successfully created."
+      redirect_to movie_path(@movie)
+    else
+      render 'new' # note, 'new' template can access @movie's field values
+    end
   end
 
   def edit
@@ -33,9 +37,12 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(params[:movie])
-    flash[:notice] = "#{@movie.title} was successfully updated."
-    redirect_to movie_path(@movie)
+    if @movie.update_attributes(params[:movie])
+      flash[:notice] = "#{@movie.title} was successfully updated."
+      redirect_to movie_path(@movie)
+    else
+      render 'edit' # note: 'edit' template can access @movie's field values
+    end
   end
 
   def destroy
