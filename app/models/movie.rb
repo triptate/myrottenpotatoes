bb66a8ad15a7bf1b24bacae2f54b6d0347acb627
Trip Tate <trip@pegulabs.com>
2014-12-06 23:23:19 +0000

    Added attribute validation to movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,25 @@
 class Movie < ActiveRecord::Base
 
-  attr_accessible :title, :rating, :description, :release_date
+  def self.all_ratings; %w[G M PG PG-13 R NC-17 X]; end # shortcut for an array of strings
 
-  def self.all_ratings
-    ['G', 'PG', 'PG-13', 'R', 'NC-17']
+  validates :title, :presence => true
+  validates :release_date, :presence => true
+  validate :released_1930_or_later # uses custom validator below
+  validates :rating, :inclusion => {:in => Movie.all_ratings}, :unless => :grandfathered?
+  validates :rating, :absence => { :message => 'must be empty if movie predates rating system'},
+  	:if => :grandfathered?
+
+  def released_1930_or_later
+  	if release_date && release_date < Date.parse('1 Jan 1930')
+  	  errors.add(:release_date, 'must be 1930 or later')
+  	end
   end
+
+  @@grandfathered_date = Date.parse('1 Nov 1968')
+  def grandfathered?
+  	release_date && release_date < @@grandfathered_date
+  end
+
+  attr_accessible :title, :rating, :description, :release_date
 
 end
