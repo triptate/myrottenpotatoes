25aac84343700ca1c51f0d5679127ebec34f0c48
Trip Tate <trip@pegulabs.com>
2014-12-09 00:31:39 +0000

    Fixed a typo

--- app/models/moviegoer.rb
+++ app/models/moviegoer.rb
@@ -3,7 +3,7 @@ class Moviegoer < ActiveRecord::Base
   attr_accessible :uid, :provider, :name
 
   def self.create_with_omniauth(auth)
-  	Movigoer.create!(
+  	Moviegoer.create!(
   		:provider => auth["provider"],
   		:uid => auth["uid"],
   		:name => auth["info"]["name"])
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "UL8bwRs7J2Py4IHNoC7mudzTr", "25c3WmnezvYUNuSSzMQ9fgPN1VVT941cBC0bu0xc0RWM6xGvYG",
-  {
-  	:image_size => 'original',
-  	
-  }
+  provider :twitter, "UL8bwRs7J2Py4IHNoC7mudzTr", "25c3WmnezvYUNuSSzMQ9fgPN1VVT941cBC0bu0xc0RWM6xGvYG"
 end
