93a103ff0be8575ab9b5fb3a4bbd5951857abfe4
Trip Tate <trip@pegulabs.com>
2015-01-25 18:55:35 +0000

    Got the third rspec test for search_tmdb to pass

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -3,20 +3,29 @@ require 'rails_helper'
 describe MoviesController do
   describe 'searching TMDb' do
 
+  	before :each do
+      @fake_results = [mock_model(Movie), mock_model(Movie)]
+  	end
+
     it 'should call the model method that performs TMDb search' do
-      fake_results = [mock_model(Movie), mock_model(Movie)]
-      expect(Movie).to receive(:find_in_tmdb).with('hardware').and_return(fake_results)
+      expect(Movie).to receive(:find_in_tmdb).with('hardware').and_return(@fake_results)
       post :search_tmdb, {:search_terms => 'hardware'}
     end
 
-    it 'should select the Search Results template for rendering' do
-      fake_results = [mock_model(Movie), mock_model(Movie)]
-      allow(Movie).to receive(:find_in_tmdb).and_return(fake_results)
-      post :search_tmdb, {:search_terms => 'hardware'}
-      expect(response).to render_template('search_tmdb')
-    end
+    describe 'after valid search' do
+      before :each do
+        allow(Movie).to receive(:find_in_tmdb).and_return(@fake_results)
+        post :search_tmdb, {:search_terms => 'hardware'}
+      end
 
-    it 'should make the TMDb search results available to that template'
+	  it 'should select the Search Results template for rendering' do
+        expect(response).to render_template('search_tmdb')
+      end
+
+      it 'should make the TMDb search results available to that template' do
+        expect(assigns(:movies)).to eq(@fake_results)
+      end
+    end
 
   end
 end
