d54cfa8daa9f71cc2ba63d34749f0ec6d8bf3e3e
Trip Tate <trip@pegulabs.com>
2015-03-03 15:22:05 +0000

    Made the sorting caret look pretty

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,8 @@ module ApplicationHelper
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    link_to title, {:sort => column, :direction => direction}, :class => css_class
+    alt_direction = direction == "asc" ? "up" : "down"
+    title << ( column == sort_column ? " <i class='fa fa-caret-#{alt_direction}'></i>" : "" )
+    link_to title.html_safe, {:sort => column, :direction => direction}, :class => css_class
   end
 end
